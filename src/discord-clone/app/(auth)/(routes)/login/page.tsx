323b0b59516bd46b453cf08e6dae2546b3626f1e
Jehee Cheon <jeheecheon@gmail.com>
2023-12-02 02:44:09 +0900

    next-auth, zod 설치 및 next 업데이트

--- src/discord-clone/app/(auth)/(routes)/login/page.tsx
+++ src/discord-clone/app/(auth)/(routes)/login/page.tsx
@@ -1,8 +1,24 @@
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+
 const login = () => {
     return (
-        <div>
-            Login Page
-        </div>
+        <p>asd</p>
+        // <Form>
+        //     <FormField
+        //         control={form.control}
+        //         name="username"
+        //         render={({ field }) => (
+        //             <FormItem>
+        //                 <FormLabel>Username</FormLabel>
+        //                 <FormControl>
+        //                     <Input placeholder="shadcn" {...field} />
+        //                 </FormControl>
+        //                 <FormDescription>This is your public display name.</FormDescription>
+        //                 <FormMessage />
+        //             </FormItem>
+        //         )}
+        //     />
+        // </Form>
     );
 }
 
--- src/discord-clone/app/(main)/(routes)/page.tsx
+++ src/discord-clone/app/(main)/(routes)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 
 export default function Home() {
@@ -9,6 +10,18 @@ export default function Home() {
           Hello Discord Clone
         </p>
         <Button>Click me</Button>
+        <Card className="w-1/2">
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
       </div>
     </>
   )
